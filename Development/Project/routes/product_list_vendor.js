cdfd52aa5c5ffef95e331038cf992d286f8e0787
rabiahasan <rabialibra8@hotmail.com>
2016-12-12 20:30:46 +0500

    Database connectivity admin panel , vendor page products insert update delete within vendor's session login

--- Development/Project/routes/product_list_vendor.js
+++ Development/Project/routes/product_list_vendor.js
@@ -1,16 +1,17 @@
 var express = require('express');
-console.log("cat 2");
+console.log("cat vendorlist");
 var router = express.Router();
 /* GET home page. */
-router.get('/subcategories', function (req, res, next) {
-console.log("her subca");
+router.get('/product_list_vendor', function (req, res, next) {
+
+    console.log("her listvendorrr");
     var data = new Object();
     console.log(req.session.username);
 
     if(req.session.username){
         data.username = req.session.username;
     }
-    res.render('vendor', data);
+    res.render('product_list_vendor', data);
 });
 
 
--- Development/Project/routes/vendor.js
+++ Development/Project/routes/vendor.js
@@ -1,17 +1,22 @@
 var express = require('express');
-console.log("cat 2");
+console.log("cat");
 var router = express.Router();
+
+console.log("777777777777777");
+
 /* GET home page. */
-router.get('/subcategories', function (req, res, next) {
-console.log("her subca");
+router.get('/vendor', function (req, res, next) {
+
+    console.log("777777777777777");
+
     var data = new Object();
     console.log(req.session.username);
-
-    if(req.session.username){
+        if(req.session.username){
         data.username = req.session.username;
     }
     res.render('vendor', data);
 });
+
 
 
 
